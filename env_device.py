e2efaf31c251c77779bf6dc09f16d0c1e8d2979e
wunderup <60374420+wunderup@users.noreply.github.com>
2020-10-30 17:37:53 +0800

    Add files via upload

---
+++ env_device.py
@@ -0,0 +1,17 @@
+# SD-WAN vManager
+vManager_HOST = "sandboxsdwan.cisco.com"
+
+# DNA Center device
+DNAC_HOST = "sandboxdnac.cisco.com"
+
+# Meraki Dashboard
+MERAKI_URL = "https://api.meraki.com/api/v0/"
+MERAKI_NETWORK_ID = "L_566327653141843049"
+
+# Webex Teams URL to send messages
+WEBEX_TEAMS_URL = "https://api.ciscospark.com/v1/messages"
+
+# E-mail server and email receiver
+EMAIL_HOST = "smtp.163.com"
+EMAIL_SEND_MAILBOX = "your_email@163.com"
+EMAIL_RECV_MAILBOX = ["notify_recv_1@cisco.com", "notify_recv_2@cisco.com"]
---
+++ env_user_credentials.py
@@ -0,0 +1,18 @@
+# SD-WAN vManager username and password
+vManager_USERNAME = "devnetuser"
+vManager_PASSWORD = "Cisco123!"
+
+# DNA Center username and password
+DNAC_USER = "devnetuser"
+DNAC_PASSWORD = "Cisco123!"
+
+# Meraki API key
+MERAKI_API_KEY = "6bec40cf957de430a6f1f2baa056b99a4fac9ea0"
+
+# E-mail username and password
+EMAIL_USERNAME = "email_username"
+EMAIL_PASSWORD = "email_password"
+
+# send message to Webex Teams Spaces - DevNetDemo
+WEBEX_TEAMS_SPACE = "webex_team_space_more_than_70_characters"
+WEBEX_TEAMS_TOKEN = "webex_teams_token_more_than_100_characters"
